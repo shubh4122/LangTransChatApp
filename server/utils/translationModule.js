f40016b4a084f39d8f072ab6b5976b2ba4f6c6b6
Shubham Pandey <shubhampandeylearning@gmail.com>
2024-01-17 00:02:06 +0530

    Added await async to translation api

--- server/utils/translationModule.js
+++ server/utils/translationModule.js
@@ -1,11 +1,12 @@
-function translate(message, senderLang, recieverLang) {
+async function translate(message, senderLang, recieverLang) {
   //-------------------- This is how we translate -----------------------
   const transText = `https://api.mymemory.translated.net/get?q=${message}&langpair=${senderLang}|${recieverLang}`;
 
-  fetch(transText)
+  return await fetch(transText)
     .then((transText) => transText.json())
     .then((data) => {
-      console.log(data.responseData.translatedText);
+      console.log(`From module : ${data.responseData.translatedText}`);
+      return data.responseData.translatedText;
     });
 }
 
